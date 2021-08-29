922a7bbe65c5e71ac570968577ed9c064ba77f5c
gymoreira <gymoreira@outlook.com>
2021-08-29 13:59:38 -0300

    colocando /

--- app/src/Models/AnalisadorLexico.py
+++ app/src/Models/AnalisadorLexico.py
@@ -2,50 +2,57 @@ from os.path import dirname, abspath
 from os import listdir
 from automato import Automato
 
-#FUNCTION TO GET ALL THE TEXTS INPUTS
+# FUNCTION TO GET ALL THE TEXTS INPUTS
+
+
 def getInputFiles(dir):
     files = []
     dir = listdir(dir)
 
-    for index,filename in enumerate(dir):
+    for index, filename in enumerate(dir):
         if filename.startswith('entrada'):
             idx = int(''.join(i for i in filename if i.isdigit()))
             print(idx)
-            files.append([filename,idx])
+            files.append([filename, idx])
 
     return files
 
-#FUNCTION TO READ THE FILE
-def readFileInputs(path,pathOutput,filename,index):
+# FUNCTION TO READ THE FILE
+
+
+def readFileInputs(path, pathOutput, filename, index):
+    #automato = Automato(path + '/' + filename)
     automato = Automato(path + '\\' + filename)
     tokens = automato.getNextToken()
     lineTxt = []
-    
-    file = open(pathOutput+'\\saida'+str(index)+'.txt','w')
-    
+
+    #file = open(pathOutput+'/saida'+str(index)+'.txt','w')
+    file = open(pathOutput+'\\saida'+str(index)+'.txt', 'w')
+
     for idx, token in enumerate(tokens['states']):
-        lineTxt= str(token.getLine()+1) + ' ' + token.getType() + ' ' + token.getWord()
+        lineTxt = str(token.getLine()+1) + ' ' + \
+            token.getType() + ' ' + token.getWord()
         file.write(lineTxt)
         file.write('\n')
-        
+
     for idx, token in enumerate(tokens['errors']):
         file.write('\n')
-        lineTxt= str(token.getLine()+1) + ' ' + token.getType() + ' ' + token.getWord()
+        lineTxt = str(token.getLine()+1) + ' ' + \
+            token.getType() + ' ' + token.getWord()
         file.write(lineTxt)
     file.close()
-        
-    
 
 
 def main():
     path = dirname(dirname(dirname(dirname(abspath(__file__)))))
+    #pathOutput = path + '/output'
+    #path = path + '/input'
     pathOutput = path + '\\output'
     path = path + '\\input'
     inputFiles = getInputFiles(path)
 
     for idx, file in enumerate(inputFiles):
         readFileInputs(path, pathOutput, file[0], file[1])
-
 
 
 main()
