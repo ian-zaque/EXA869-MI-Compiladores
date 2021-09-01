3d86be27a74cc68abf3d974dce387c65fdb8a8f1
gymoreira <gymoreira@outlook.com>
2021-09-01 07:39:19 -0300

    add caminho em windows

--- app/src/Models/AnalisadorLexico.py
+++ app/src/Models/AnalisadorLexico.py
@@ -8,7 +8,9 @@ from automato import Automato
 
 def readFileInputs(path, pathOutput, filename, index):
     tokens = Automato(path + '\\' + filename).getTokens()
-    FileWriter.write(path, pathOutput, filename, index,tokens)
+    #tokens = Automato(path + '/' + filename).getTokens()
+    FileWriter.write(path, pathOutput, filename, index, tokens)
+
 
 def main():
     # os.chdir(os.path.abspath(
@@ -16,13 +18,16 @@ def main():
     path = dirname(dirname(dirname(dirname(abspath(__file__)))))
     pathOutput = path + '\\output'
     pathInput = path + '\\input'
-    
+    #pathOutput = path + '/output'
+    #pathInput = path + '/input'
+
     try:
         inputFiles = FileReader.getInputFiles(pathInput)
         for idx, file in enumerate(inputFiles):
             readFileInputs(pathInput, pathOutput, file[0], file[1])
-        
+
     except OSError:
         print("Erro ao escanear e analisar um dos arquivos")
+
 
 main()
