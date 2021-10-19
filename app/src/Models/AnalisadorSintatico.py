86bf70e620d7e8c85e41efe7e6e7577072b80805
Ian Zaque Santos <43974640+ian-zaque@users.noreply.github.com>
2021-10-19 01:28:33 -0300

    reconhecendo variaveis

--- app/src/Models/AnalisadorSintatico.py
+++ app/src/Models/AnalisadorSintatico.py
@@ -31,8 +31,11 @@ class AnalisadorSintatico:
         else:
             return Token("EOF","EOF",-1)
     
+    def isPrimitiveType(self,word):
+        return word in self.primitives_types
+    
     def parse(self):
-        #   <declaracao_reg> <declaration_const> <declaration_var> <function_declaration> | <var_atr> | <expressao>
+        #<Program> ::= <declaracao_reg> <declaration_const> <declaration_var> <function_declaration> | <var_atr> | <expressao>
         self.getNextToken()
         self.start()
     
@@ -52,6 +55,9 @@ class AnalisadorSintatico:
             return
         
         elif self.counter < len(self.tokens):
+            print('CONSTANTES_0',self.palavra)
+            print('TOKEN_0',self.getToken().getWord())
+            
             if self.getToken().getType() == 'PRE' and self.getToken().getWord() == 'constantes':
                 self.palavra = self.palavra + self.getToken().getWord() + '$'
                 self.getNextToken()
@@ -62,7 +68,7 @@ class AnalisadorSintatico:
                 self.getNextToken()
                 return self.declaracao_const()
                 
-            elif self.getToken().getType() == 'PRE' and self.getToken().getWord() in self.primitives_types:
+            elif self.getToken().getType() == 'PRE' and self.isPrimitiveType(self.getToken().getWord()):
                 self.palavra = self.palavra + self.getToken().getWord() + '$'
                 self.getNextToken()
                 return self.declaracao_const1()
@@ -79,7 +85,8 @@ class AnalisadorSintatico:
             return
         
         elif self.counter < len(self.tokens):
-            print('PALAVRA 1',self.palavra)
+            print('CONSTANTES_1',self.palavra)
+            print('TOKEN_1',self.getToken().getWord())
             
             #FIRST DERIV.
             if self.getToken().getType() == 'IDE':
@@ -121,7 +128,8 @@ class AnalisadorSintatico:
             return
         
         elif self.counter < len(self.tokens):
-            print('PALAVRA 2',self.palavra)
+            print('CONSTANTES_2',self.palavra)
+            print('TOKEN_2',self.getToken().getWord())
             
             #FIRST DERIV.
             if self.getToken().getType() == 'IDE':
@@ -154,76 +162,65 @@ class AnalisadorSintatico:
                 #erro
                 print('erro 2',self.palavra)
                 # self.getNextToken()
-    
+                
     # <declaration_var>  ::= variaveis '{' <declaration_var1>
     def declaracao_var(self):
         if self.getToken().getType() == 'EOF':
             return
         
         elif self.counter < len(self.tokens):
-            print('PALAVRA 3',self.palavra, self.getToken().getWord())
-        
-            #first deriv.
+            print('VARIAVEIS_0',self.palavra)
+            print('TOKEN_0',self.getToken().getWord())
+            
             if self.getToken().getType() == 'PRE' and self.getToken().getWord() == 'variaveis':
                 self.palavra = self.palavra + self.getToken().getWord() + '$'
                 self.getNextToken()
                 return self.declaracao_var()
-                
-            if self.getToken().getType() == 'DEL' and (self.getToken().getWord() == '{' or self.getToken().getWord() == ','):
+                            
+            elif self.getToken().getType() == 'DEL' and self.getToken().getWord() == '{':
                 self.palavra = self.palavra + self.getToken().getWord() + '$'
                 self.getNextToken()
                 return self.declaracao_var()
-            
-            elif (self.getToken().getWord() in self.primitives_types) or (self.getToken().getType() == 'IDE'):
-                self.palavra = self.palavra + self.getToken().getWord() + '$'
-                self.getNextToken()
-                return self.declaracao_var1()
 
-            else:
-                #erro
-                print('erro 3',self.palavra)
-                # self.getNextToken()
-        
-    # <declaration_var1> ::= <type> id <declaration_var2> | '}'
-    def declaracao_var1(self):
-        if self.getToken().getType() == 'EOF':
-            return
-        
-        elif self.counter < len(self.tokens):
-            print('PALAVRA 4',self.palavra,self.getToken().getWord())
-        
-            #first deriv.
-            if self.getToken().getType() == 'IDE':
+            elif self.isPrimitiveType(self.getToken().getWord()) or self.getToken().getType() == 'IDE':
                 self.palavra = self.palavra + self.getToken().getWord() + '$'
                 self.getNextToken()
                 return self.declaracao_var1()
             
-            if self.getToken().getType() == 'DEL' and self.getToken().getWord() == ';':
-                self.palavra = self.palavra + self.getToken().getWord() + '$'
-                self.getNextToken()
-                return self.declaracao_var1()
-            
-            if self.getToken().getType() == 'DEL' and self.getToken().getWord() == ',':
-                self.palavra = self.palavra + self.getToken().getWord() + '$'
-                self.getNextToken()
-                return self.declaracao_var3()
-                
-            if self.getToken().getType() == 'REL':
-                self.palavra = self.palavra + self.getToken().getWord() + '$'
-                self.getNextToken()
-                return self.declaracao_var2()
-            
-            # SECOND DERIV.
-            elif self.getToken().getType() == 'DEL' and self.getToken().getWord() == '}':
-                #FIM
-                self.palavra = self.palavra + self.getToken().getWord() + '$'
+            elif self.getToken().getWord() == '}':
                 print('fim_variaveis',self.palavra, '\n')
                 self.palavra = ''
                 return
             
             else:
                 #erro
-                print('erro 4',self.palavra)
+                print('erro_variaveis_0',self.palavra)
+                # self.getNextToken()
+    
+    # <declaration_var1> ::= <type> id <declaration_var2> | '}'
+    def declaracao_var1(self):
+        if self.getToken().getType() == 'EOF':
+            return
+        
+        elif self.counter < len(self.tokens):
+            print('VARIAVEIS_1',self.palavra)
+            print('TOKEN_1',self.getToken().getWord())
+
+            #FIRST DERIV.
+            if self.getToken().getType() == 'IDE':
+                self.palavra = self.palavra + self.getToken().getWord() + '$'
+                self.getNextToken()
+                return self.declaracao_var1()
+
+            #add teste de vetor e matriz
+            elif self.getToken().getWord() == '=' or self.getToken().getWord() == ',' or self.getToken().getWord() == ';':
+                self.palavra = self.palavra + self.getToken().getWord() + '$'
+                self.getNextToken()
+                return self.declaracao_var2()
+        
+            else:
+                #erro
+                print('erro_variaveis_1',self.palavra)
                 # self.getNextToken()
     
     # <declaration_var2> ::= '=' <value> <declaration_var3> | <vector_matrix> | <declaration_var3>
@@ -232,28 +229,32 @@ class AnalisadorSintatico:
             return
         
         elif self.counter < len(self.tokens):
-            print('PALAVRA 5',self.palavra)
-            
-            #first deriv.
-            if self.getToken().getType() == 'NRO' or self.getToken().getType() == 'CAD' or self.getToken().getType() == 'PRE':
+            print('VARIAVEIS_2',self.palavra)
+            print('TOKEN_2',self.getToken().getWord())
+
+            #FIRST DERIV.
+            if self.getToken().getType() == 'NRO' or self.getToken().getType() == 'CAD' or self.getToken().getType() == 'PRE' or self.getToken().getType() == 'IDE':
                 self.palavra = self.palavra + self.getToken().getWord() + '$'
                 self.getNextToken()
-                return self.declaracao_var3()
-                
-            #SECOND DERIV.
-            # elif token:
-            #     self.palavra = token.getWord() + self.palavra
-            #     #GO TO DEF. VECTOR_MATRIX
+                return self.declaracao_var2()
             
-            #THIRD DERIV.
+            elif self.getToken().getWord() == '=':
+                self.palavra = self.palavra + self.getToken().getWord() + '$'
+                self.getNextToken()
+                return self.declaracao_var2()
+            
+            #SECOND DERIV.
+            # TESTAR INICIO DE MATRIZ E VETOR AQUI
+            
+            #LAST PART OF FIRST DERIV. AND THIRD DERIV. 
             elif self.getToken().getType() == 'DEL' and (self.getToken().getWord() == ',' or self.getToken().getWord() == ';'):
                 self.palavra = self.palavra + self.getToken().getWord() + '$'
                 self.getNextToken()
                 return self.declaracao_var3()
-                
+            
             else:
                 #erro
-                print('erro 5',self.palavra)
+                print('erro_variaveis_2',self.palavra)
                 # self.getNextToken()
     
     # <declaration_var3> ::= ',' id <declaration_var2>  | ';' <declaration_var1>
@@ -262,22 +263,163 @@ class AnalisadorSintatico:
             return
         
         elif self.counter < len(self.tokens):
-            print('PALAVRA 6',self.palavra)
-            
-            #FRIST DERIV.
+            print('VARIAVEIS_3',self.palavra)
+            print('TOKEN_3',self.getToken().getWord())
+
+            #FIRST DERIV.
             if self.getToken().getType() == 'IDE':
+                self.palavra = self.palavra + self.getToken().getWord() + '$'
+                self.getNextToken()
+                return self.declaracao_var3()
+            
+            #add teste de matriz/vetor aqui para declaracao var 2
+            elif self.getToken().getWord() == '=' or self.getToken().getWord() == ',' or self.getToken().getWord() == ';':
                 self.palavra = self.palavra + self.getToken().getWord() + '$'
                 self.getNextToken()
                 return self.declaracao_var2()
             
             #SECOND DERIV.
-            elif self.getToken().getType() == 'PRE':
+            elif (self.isPrimitiveType(self.getToken().getWord()) or self.getToken().getType() == 'IDE'):
                 self.palavra = self.palavra + self.getToken().getWord() + '$'
                 self.getNextToken()
                 return self.declaracao_var1()
             
+            elif self.getToken().getWord() == '}':
+                self.palavra = self.palavra + self.getToken().getWord() + '$'
+                return self.declaracao_var()
+            
             else:
                 #erro
-                print('erro 6',self.palavra)
+                print('erro_variaveis_2',self.palavra)
                 # self.getNextToken()
+            
+    # # <declaration_var>  ::= variaveis '{' <declaration_var1>
+    # def declaracao_var(self):
+    #     if self.getToken().getType() == 'EOF':
+    #         return
+        
+    #     elif self.counter < len(self.tokens):
+    #         print('VARIAVEIS_0',self.palavra)
+    #         print('TOKEN_0',self.getToken().getWord())
+        
+    #         #first deriv.
+    #         if self.getToken().getType() == 'PRE' and self.getToken().getWord() == 'variaveis':
+    #             self.palavra = self.palavra + self.getToken().getWord() + '$'
+    #             self.getNextToken()
+    #             return self.declaracao_var()
+                
+    #         if self.getToken().getType() == 'DEL' and (self.getToken().getWord() == '{' or self.getToken().getWord() == ','):
+    #             self.palavra = self.palavra + self.getToken().getWord() + '$'
+    #             self.getNextToken()
+    #             return self.declaracao_var()
+            
+    #         elif (self.isPrimitiveType(self.getToken().getWord())) or (self.getToken().getType() == 'IDE'):
+    #             self.palavra = self.palavra + self.getToken().getWord() + '$'
+    #             self.getNextToken()
+    #             return self.declaracao_var1()
+
+    #         else:
+    #             #erro
+    #             print('erro 3',self.palavra)
+    #             # self.getNextToken()
+        
+    # # <declaration_var1> ::= <type> id <declaration_var2> | '}'
+    # def declaracao_var1(self):
+    #     if self.getToken().getType() == 'EOF':
+    #         return
+        
+    #     elif self.counter < len(self.tokens):
+    #         print('VARIAVEIS_1',self.palavra)
+    #         print('TOKEN_1',self.getToken().getWord())
+        
+    #         #first deriv.
+    #         if self.getToken().getType() == 'IDE':
+    #             self.palavra = self.palavra + self.getToken().getWord() + '$'
+    #             self.getNextToken()
+    #             return self.declaracao_var1()
+            
+    #         if self.getToken().getType() == 'DEL' and self.getToken().getWord() == ';':
+    #             self.palavra = self.palavra + self.getToken().getWord() + '$'
+    #             self.getNextToken()
+    #             return self.declaracao_var3()
+            
+    #         if self.getToken().getType() == 'DEL' and self.getToken().getWord() == ',':
+    #             self.palavra = self.palavra + self.getToken().getWord() + '$'
+    #             self.getNextToken()
+    #             return self.declaracao_var3()
+                
+    #         if self.getToken().getType() == 'REL':
+    #             self.palavra = self.palavra + self.getToken().getWord() + '$'
+    #             self.getNextToken()
+    #             return self.declaracao_var2()
+            
+    #         # SECOND DERIV.
+    #         elif self.getToken().getType() == 'DEL' and self.getToken().getWord() == '}':
+    #             #FIM
+    #             self.palavra = self.palavra + self.getToken().getWord() + '$'
+    #             print('fim_variaveis',self.palavra, '\n')
+    #             self.palavra = ''
+    #             return
+            
+    #         else:
+    #             #erro
+    #             print('erro 4',self.palavra)
+    #             # self.getNextToken()
+    
+    # # <declaration_var2> ::= '=' <value> <declaration_var3> | <vector_matrix> | <declaration_var3>
+    # def declaracao_var2(self):
+    #     if self.getToken().getType() == 'EOF':
+    #         return
+        
+    #     elif self.counter < len(self.tokens):
+    #         print('VARIAVEIS_2',self.palavra)
+    #         print('TOKEN_2',self.getToken().getWord())
+            
+    #         #first deriv.
+    #         if self.getToken().getType() == 'NRO' or self.getToken().getType() == 'CAD' or self.getToken().getType() == 'PRE':
+    #             self.palavra = self.palavra + self.getToken().getWord() + '$'
+    #             self.getNextToken()
+    #             return self.declaracao_var3()
+                
+    #         #SECOND DERIV.
+    #         # elif token:
+    #         #     self.palavra = token.getWord() + self.palavra
+    #         #     #GO TO DEF. VECTOR_MATRIX
+            
+    #         #THIRD DERIV.
+    #         elif self.getToken().getType() == 'DEL' and (self.getToken().getWord() == ',' or self.getToken().getWord() == ';'):
+    #             self.palavra = self.palavra + self.getToken().getWord() + '$'
+    #             self.getNextToken()
+    #             return self.declaracao_var3()
+                
+    #         else:
+    #             #erro
+    #             print('erro 5',self.palavra)
+    #             # self.getNextToken()
+    
+    # # <declaration_var3> ::= ',' id <declaration_var2>  | ';' <declaration_var1>
+    # def declaracao_var3(self):
+    #     if self.getToken().getType() == 'EOF':
+    #         return
+        
+    #     elif self.counter < len(self.tokens):
+    #         print('VARIAVEIS_3',self.palavra)
+    #         print('TOKEN_3',self.getToken().getWord())
+            
+    #         #FRIST DERIV.
+    #         if self.getToken().getType() == 'IDE':
+    #             self.palavra = self.palavra + self.getToken().getWord() + '$'
+    #             self.getNextToken()
+    #             return self.declaracao_var2()
+            
+    #         #SECOND DERIV.
+    #         elif self.getToken().getType() == 'PRE':
+    #             self.palavra = self.palavra + self.getToken().getWord() + '$'
+    #             self.getNextToken()
+    #             return self.declaracao_var1()
+            
+    #         else:
+    #             #erro
+    #             print('erro 6',self.palavra)
+    #             # self.getNextToken()
                 
